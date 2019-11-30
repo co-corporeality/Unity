c37f589dfea15f6c568c6b9fc9ce06c707f782b4
Atair <28533692+Atair1@users.noreply.github.com>
2019-11-30 17:22:20 +0100

    VertexManipulation
    initial commit, nothing useful right now

---
+++ VertexManipulation/Assets/ForceField/FieldGizmo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldGizmo : MonoBehaviour
+{
+    public float radius = 1.0f;
+    public float strength = 1.0f;
+    public int id = -1;
+    
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+
+       void OnDrawGizmos() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(gameObject.transform.position, radius);
+    }
+}
---
+++ VertexManipulation/Assets/ForceFieldMesh.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class ForceFieldMesh : MonoBehaviour
+{
+    GameObject[] objects;   // subobjects to be manipulated
+    Mesh[] original;        // meshes before maniulation
+    SimplexNoise noise;
+
+    public GameObject centerDummy;
+    public float radius;
+    public float noiseScale = 0.01f;
+    public float noiseSpeed = 0.1f;
+
+    void Start() {
+        objects = GetChildren(gameObject);
+        original = SaveOriginal(objects);
+        noise = new SimplexNoise("12345");
+    }
+
+    
+    void Update() {
+        DeformMeshes(objects);
+    }
+
+    void DeformMeshes(GameObject[] objs) {
+        for(int i = 0; i < objs.Length; i++) {
+            Mesh mesh = objs[i].GetComponent<MeshFilter>().sharedMesh;
+            Vector3[] vertices = mesh.vertices;
+            Vector3[] normals = mesh.normals;
+            for(int j = 0; j < vertices.Length; j++) {
+                float n = noise.coherentNoise(vertices[j].x * noiseScale,vertices[j].y * noiseScale, vertices[j].z * noiseScale);
+                vertices[j] += noiseSpeed * normals[j] * n;
+            }
+            mesh.vertices = vertices;
+            mesh.RecalculateBounds();
+            mesh.RecalculateNormals();
+            //mesh.UploadMeshData(false);
+        }
+    }
+
+    Mesh[] SaveOriginal(GameObject[] objs) {
+        Mesh[] result = new Mesh[objs.Length];
+         for(int i = 0; i < objs.Length; i++) {
+            result[i] = Instantiate(objs[i].GetComponent<MeshFilter>().sharedMesh);
+         }
+         return result;
+    }
+
+    GameObject[] GetChildren(GameObject obj) {
+        GameObject[] result = new GameObject[obj.transform.childCount];
+        for(int i = 0; i < obj.transform.childCount; i++) {
+            result[i] = obj.transform.GetChild(i).gameObject;
+        }
+        return result;
+    }
+
+    public void Reset() {
+        Debug.Log("ok");
+        for(int i = 0; i < objects.Length; i++) {
+            objects[i].GetComponent<MeshFilter>().sharedMesh.vertices = original[i].vertices;
+        }
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(centerDummy.transform.position, radius);
+    }
+}
+
+[CustomEditor(typeof(ForceFieldMesh))]
+public class LevelScriptEditor : Editor  {
+    public override void OnInspectorGUI(){
+        ForceFieldMesh data = (ForceFieldMesh)target;
+        if(GUILayout.Button("Reset")) {
+            data.Reset();
+        }
+        DrawDefaultInspector();
+    }
+}
