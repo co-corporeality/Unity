c5f7164d1bc617ded165f1567dab8939eb5d45ce
Atair <28533692+Atair1@users.noreply.github.com>
2019-12-03 09:38:06 +0100

    Master Scene with Lichthof and OSC ForceFieds

---
+++ cocoWorkshop_MD/Assets/ForceField/Internal/FieldGizmo.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldGizmo : MonoBehaviour
+{
+    GizmoOSC gizmoOSC;
+    [Header("General Settings")]
+    [Range(0.0f, 1.0f)]
+    public float noise; 
+    public float radius = 1.0f;
+    public float strength = 1.0f;
+    public bool overrideOSC;
+
+    [HideInInspector]
+    public float realStrength;  // user multiplier * osc data
+    [HideInInspector]
+    public  float poseStrength;
+    [Header("Pose Settings")]
+    [Range(0, 17)]
+    public int id = 0;
+    [Range(0, 17)]
+    public int rootId = 0;
+    
+
+    [Header("Face Settings")]
+    [Range(0,4)]
+    public int emotionId;
+    [HideInInspector]
+    public float emotionStrength;
+    void Start() {
+        gizmoOSC = GameObject.Find("OSC").GetComponent<GizmoOSC>();
+    }
+
+    void Update() {
+        if(overrideOSC) {
+            realStrength = strength;
+        } else {
+            if(gizmoOSC.mode.Equals("POSE")) {
+                realStrength = strength * poseStrength;
+            }
+            if(gizmoOSC.mode.Equals("FACE")) {
+                //Debug.Log(emotionStrength);
+                realStrength = strength * emotionStrength;
+            }
+        }
+
+    }
+
+       void OnDrawGizmos() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(gameObject.transform.position, radius);
+    }
+}
---
+++ cocoWorkshop_MD/Assets/ForceField/Internal/GizmoOSC.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GizmoOSC : MonoBehaviour
+{
+    [HideInInspector]
+    public string mode;
+    public OSC osc;
+    [HideInInspector]
+    public float[] rawData;
+    [HideInInspector]
+    public Vector3[] rawPositions;    
+    [HideInInspector]
+    public float[] rawEmotions;
+    FieldGizmo[] gizmos; 
+    //public Mesh dummy; 
+    
+
+    void Start()
+    {
+        gizmos = GetFieldGizmos();
+        if(mode.Equals("POSE")) {
+            osc.SetAddressHandler("/pose", newPose);
+        } else {
+            osc.SetAddressHandler("/face", newFace);
+        }
+        
+        rawPositions = new Vector3[18];
+        rawEmotions = new float[5];
+    }
+
+    void Update()
+    {        
+        // dummy Preview
+        //for(int i = 0; i < rawPositions.Length; i++) {
+        //    Graphics.DrawMesh(dummy, rawPositions[i] * 20.0f, Quaternion.identity, null, 0);            
+        //} 
+        if(mode.Equals("POSE")) {
+            setPoseGizmos(); 
+        }
+        if(mode.Equals("FACE")) {
+            setFaceGizmos();
+        }
+        
+    }
+
+    void setFaceGizmos() {
+        for(int i = 0; i < gizmos.Length; i++) {
+            int id = gizmos[i].emotionId;
+            if(id > -1 && id < 5 && rawEmotions[id] > 0 ) {
+                gizmos[i].emotionStrength = rawEmotions[id];
+            }
+        }
+    }
+
+    void setPoseGizmos() {
+        for(int i = 0; i < gizmos.Length; i++) {
+            int id = gizmos[i].id;
+            int root = gizmos[i].rootId;
+            if(id > -1 && id < 18 && rawPositions[id].x > 0 && rawPositions[id].y > 0) {
+                float dist = Vector3.Distance(rawPositions[root], rawPositions[id]);
+                gizmos[i].poseStrength = dist;                
+            }
+        }
+    }
+
+    void newPose(OscMessage message)
+    {        
+        rawData = (float[])message.values.ToArray(typeof(float));
+        int c = 0;
+        for(int i = 1; i < rawData.Length; i += 2) {
+            rawPositions[c] = new Vector3(rawData[i], 1.0f - rawData[i + 1], 0);
+            c++;
+        }
+    }
+
+    void newFace(OscMessage message)
+    {        
+        System.Object[] rawData = (System.Object[])message.values.ToArray(typeof(System.Object));
+        int c = 0;
+        //string em = "";
+        for(int i = 4; i < 13; i +=2) {
+            rawEmotions[c] = (float)rawData[i];
+            c++;
+        }
+        //Debug.Log(c);
+        //for(int i = 0; i < rawData.Length; i ++) {
+        //    em += rawData[i].ToString() + " ";
+        //}
+        //Debug.Log(em);
+    }
+
+    FieldGizmo[] GetFieldGizmos() // finds all Gizmos in the scene
+    { 
+        List<FieldGizmo> result = new List<FieldGizmo>();
+        GameObject[] obj = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
+        for (var i = 0; i < obj.Length; i++)
+        {
+            FieldGizmo temp = obj[i].GetComponentInChildren<FieldGizmo>();
+            if (temp != null)
+            {
+                result.Add(temp);
+            }
+        }
+        return result.ToArray();
+    }
+}
+
+[CustomEditor(typeof(GizmoOSC))]
+ public class SomeEditor : Editor
+ {
+   string[] _choices = new [] { "POSE", "FACE" };
+   int _choiceIndex = 0;
+ 
+   public override void OnInspectorGUI ()
+   {
+       var GizmoOSC = target as GizmoOSC;
+        _choiceIndex = System.Array.IndexOf(_choices, GizmoOSC.mode);
+        _choiceIndex = EditorGUILayout.Popup(_choiceIndex, _choices);
+    
+        GizmoOSC.mode = _choices[_choiceIndex];
+  
+     DrawDefaultInspector();
+     EditorUtility.SetDirty(target);
+   }
+ }
